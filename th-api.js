8f160d1ea7516d78ef8584b95d5937563e5f00e7
cchristou3 <cchristou3@uclan.ac.uk>
2018-11-16 19:10:52 +0200

    Created js file and connected it to the html files

---
+++ th-api.js
@@ -0,0 +1,29 @@
+
+var challengesList = document.getElementById("challenges");
+function getChallenges() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.responseText);
+            var object = JSON.parse(this.responseText);
+            console.log("STATUS ==> "+object.status);
+            for (var i = 0;i<object.treasureHunts.length;i++)
+            {
+                var newItem = document.createElement("li");
+                var linkItem = document.createElement("a");
+                linkItem.innerHTML = object.treasureHunts[i].name;
+                linkItem.href = "https://codecyprus.org/th/api/start?player=Homer&app=simpsons&treasure-hunt-id="+object.treasureHunts[i].uuid; //TODO REPLACE
+                newItem.appendChild(linkItem);
+                challengesList.appendChild(newItem);
+
+                //  console.log(object.treasureHunts[i].name);
+            }
+        }
+        else {
+            //TODO If response not received (error).
+        }
+    };
+    xhttp.open("Get", "https://codecyprus.org/th/api/list", true);
+    xhttp.send();
+}
+getChallenges();
