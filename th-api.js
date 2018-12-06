82daf754cc153a200ea540f6498c579836849b7f
cchristou3 <cchristou3@uclan.ac.uk>
2018-12-06 17:52:15 +0200

    testing

--- th-api.js
+++ th-api.js
@@ -83,7 +83,6 @@ function getQuestions() {
             if (object['completed'] === true)
             {
                 //https://stackoverflow.com/questions/2144386/how-to-delete-a-cookie
-                setCookie()
                 document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
                 window.location.href = "leaderboard.html";
             }
